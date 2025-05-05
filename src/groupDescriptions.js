eaca7b5e1fab4a47bc62b759993b25bc587b4e77
Justin Rogers <65934068+JustinRyukiRogers@users.noreply.github.com>
2025-05-05 18:41:05 -0400

    Create groupDescriptions.js for popup

---
+++ src/groupDescriptions.js
@@ -0,0 +1,14 @@
+// src/descriptions.js
+
+const groupDescriptions = {
+    Contribution: "Contribution refers to the token's role in enabling the holder to contribute work or resources, either to the system itself (endogenous) or to external systems (exogenous).",
+    Governance: "Governance captures whether the token gives holders rights to participate in decisions, either within the system or in external systems.",
+    "Value Distribution": "Value distribution denotes whether the token enables the holder to receive value from the system (endogenous) or external sources (exogenous).",
+    Membership: "Membership indicates whether the token provides access or status within the community or ecosystem.",
+    Payments: "Payments refer to the token’s use as a medium of exchange or unit of account either natively or externally.",
+    Collateral: "Collateral reflects whether the token can be used to secure assets, take on leverage, or support other systems’ security.",
+    "Asset Ownership": "Asset ownership means the token confers claims on system-owned or externally managed assets.",
+  };
+  
+  export default groupDescriptions;
+  
